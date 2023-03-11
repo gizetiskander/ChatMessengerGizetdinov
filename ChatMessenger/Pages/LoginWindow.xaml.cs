85624cecd2c4df69c130a2c7b3743edc4c66e517
Гизетдинов Искандер <gizetiskander@gmail.com>
2023-03-11 08:50:14 +0300

    LoginWindow is done

--- ChatMessenger/Pages/LoginWindow.xaml.cs
+++ ChatMessenger/Pages/LoginWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ChatMessenger.db;
 
 namespace ChatMessenger.Pages
 {
@@ -19,6 +20,8 @@ namespace ChatMessenger.Pages
     /// </summary>
     public partial class LoginWindow : Window
     {
+        public static Chat_dbEntities dbEntities = new Chat_dbEntities();
+        public static Employee employee;
         public LoginWindow()
         {
             InitializeComponent();
@@ -26,12 +29,34 @@ namespace ChatMessenger.Pages
 
         private void LogBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (UserNameTB.Text == "" || PasswordTB.Password == "")
+            {
+                MessageBox.Show("Введите данные!");
+            }
+            foreach(var emp in dbEntities.Employee)
+            {
+                if (emp.UserName == UserNameTB.Text.Trim())
+                {
+                    if (emp.Password == PasswordTB.Password.Trim())
+                    {
+                        MessageBox.Show($"Добро пожаловать - {emp.Name}");
+                        employee = emp;
+                        ChitChatTopicWindow chatTopicWindow = new ChitChatTopicWindow();
+                        chatTopicWindow.Show();
+                        this.Close();
+                    }
+                }
+                else if (emp.UserName != UserNameTB.Text.Trim() || emp.Password != PasswordTB.Password.Trim())
+                {
+                    MessageBox.Show("Неверные данные!");
+                    return;
+                }
+            }
         }
 
         private void CancelBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            Environment.Exit(0);
         }
     }
 }
