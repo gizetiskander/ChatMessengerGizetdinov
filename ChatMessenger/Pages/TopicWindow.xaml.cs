3752677637a2d5319d91bad48042ba3d8a1252df
Гизетдинов Искандер <gizetiskander@gmail.com>
2023-03-11 08:40:31 +0300

    LoginWindow front is end

--- ChatMessenger/Pages/TopicWindow.xaml.cs
+++ ChatMessenger/Pages/TopicWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChatMessenger
+namespace ChatMessenger.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для TopicWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TopicWindow : Window
     {
-        public MainWindow()
+        public TopicWindow()
         {
             InitializeComponent();
         }
